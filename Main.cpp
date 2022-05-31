370f675dcabf8b6f362353b4d0cd5eea96b0a67a
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2022-05-31 19:06:31 +0900

    비동기 입력키 기능 추가, 코드 함수화
    비동기 입력키 기능 (20213091 최지원)
    - 방향키 (Snake 움직임)
    - ESC (게임 종료)
    코드 함수화 (20193216 장수미)
    - void using_color()
    - void init()
    - void init_draw()
    
    Co-Authored-By: jangsumi <104475363+jangsumi@users.noreply.github.com>

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <ncurses.h>
 #include <fstream>
+#include <vector>
+#include <thread>
 
 #define WIDTH_GB 25        // GameBoard 가로 길이
 #define HEIGHT_GB 25       // GameBoard 세로 길이
@@ -11,7 +13,20 @@
 
 using namespace std;
 
+int x = 11, y = 12;
+
+vector<pair<int, int> > snake;
+
 char map[25][25];
+
+bool running = true;
+
+
+WINDOW *GameBoard;
+WINDOW *ScoreBoard;
+WINDOW *MissionBoard;
+
+static int key = KEY_LEFT; 
 
 void LoadStage(int stage)
 {
@@ -19,39 +34,79 @@ void LoadStage(int stage)
     fin.open("data/map/stage_0.txt");
 
     char temp[WIDTH_GB + 1];
-    for (int y = 0; y < WIDTH_GB; y++)
+    for (int i = 0; i < WIDTH_GB; i++)
     {
         fin.getline(temp, 26);
-        for (int x = 0; x < HEIGHT_GB; x++)
+        for (int j = 0; j < HEIGHT_GB; j++)
         {
-            map[y][x] = temp[x];
+            map[i][j] = temp[j];
         }
     }
     fin.close();
 }
-
-
-int main()
+void keyControl()
 {
-    LoadStage(1);
+    int input;
 
-    cout << "실행됨" << endl;
+    while(running)
+    {
+        input = getch();
 
-    WINDOW *GameBoard;
-    WINDOW *ScoreBoard;
-    WINDOW *MissionBoard;
-
-    initscr();                                  // Curses 모드 시작
-
-    curs_set(0);                                // 커서 제거 (커서 깜빡임)
-
-    resize_term(HEIGHT_GB + 4, WIDTH_GB * 2);
+        if(input == KEY_UP || input == KEY_DOWN || input == KEY_LEFT || input == KEY_RIGHT)
+        {
+        // 입력 진행과 반대 방향의 키를 입력하면 게임 종료
+            if((input == KEY_UP && key == KEY_DOWN) || (input == KEY_DOWN && key == KEY_UP)
+                || (input == KEY_LEFT && key == KEY_RIGHT) || (input == KEY_RIGHT && key == KEY_LEFT))
+                {
+                running = false;
+                }
+            key = input;
+        }
+        else if(input == 27)
+        {
+        running = false;
+        }
+    }
+}
+void using_color()
+{
     start_color();                              // Color 사용 선언 <- 사용 전에 무조건 선언!
     init_pair(1, COLOR_WHITE, COLOR_WHITE);
     init_pair(2, COLOR_BLACK, COLOR_BLACK);
     init_pair(3, COLOR_WHITE, COLOR_RED);
     init_pair(4, COLOR_WHITE, COLOR_BLUE);
     init_pair(5, COLOR_GREEN, COLOR_GREEN);
+    init_pair(6, COLOR_RED, COLOR_RED);
+}
+void init()
+{
+
+    initscr();                                  // Curses 모드 시작
+    keypad(stdscr, TRUE);
+    curs_set(0);                                // 커서 제거 (커서 깜빡임)
+    noecho();
+    timeout(0);
+    resize_term(HEIGHT_GB + 4, WIDTH_GB * 2);
+    
+    using_color();
+
+    // Snake 기본 설정
+    map[y][x] = 3;
+    snake.push_back(pair<int, int>(y, x + 1));
+    snake.push_back(pair<int, int>(y, x + 2));
+
+    for (int i = 0; i < snake.size(); i++)
+    {
+        map[snake[i].first][snake[i].second] = 4;
+    }
+}
+
+void init_draw()
+{
+    delwin(GameBoard);
+    delwin(ScoreBoard);
+    delwin(MissionBoard);
+    endwin();
 
     mvprintw(1, 2, "GAME BOARD");
     mvprintw(1, 29, "SCORE BOARD");
@@ -62,7 +117,7 @@ int main()
     refresh();
 
     GameBoard = newwin(HEIGHT_GB, WIDTH_GB, 2, 2); // 행 크기, 열 크기, 시작 y좌표, 시작 x좌표
-    for(int i = 0; i < HEIGHT_GB; i++)
+    for (int i = 0; i < HEIGHT_GB; i++)
     {
         for(int j = 0; j < WIDTH_GB; j++)
         {
@@ -95,14 +150,90 @@ int main()
     MissionBoard = newwin(HEIGHT_SMB, WIDTH_SMB, 15, 29);
     wbkgd(MissionBoard, COLOR_PAIR(4));
 
+    refresh();
     wrefresh(GameBoard);
     wrefresh(ScoreBoard);
     wrefresh(MissionBoard);
+}
 
-    getch();
-    endwin();
+
+int main()
+{
+    LoadStage(1);
+
+    init();
+    init_draw();
+
+    int tmp_x, tmp_y;
+    thread key_thread(keyControl);
+
+    while (running)
+    {   
+        map[y][x] = '0';
+        for (int i = 0; i < snake.size(); i++)
+        {
+            map[snake[i].first][snake[i].second] = '0';
+        }
+
+        tmp_y = snake[snake.size() - 1].first;
+        tmp_x = snake[snake.size() - 1].second;
+
+        for(int i = snake.size() - 1; i > 0; i--)
+        {
+            snake[i] = snake[i-1];
+        }
+        snake[0].first = y;
+        snake[0].second = x;
+
+        if (key == KEY_LEFT)
+            x--;
+        else if (key == KEY_RIGHT)
+            x++;
+        else if (key == KEY_UP)
+            y--;
+        else if (key == KEY_DOWN)
+            y++;
+        
+        map[y][x] = '3';
+
+        for (int i = 0; i < snake.size(); i++)
+        {
+            map[snake[i].first][snake[i].second] = '4';
+        }
+
+        refresh();
+        
+        GameBoard = newwin(HEIGHT_GB, WIDTH_GB, 2, 2); // 행 크기, 열 크기, 시작 y좌표, 시작 x좌표
+
+    for(int i = 0; i < HEIGHT_GB; i++)
+    {
+        for(int j = 0; j < WIDTH_GB; j++)
+        {
+            switch (map[i][j])
+            {
+                case '0':
+                    mvwaddch(GameBoard, i, j, ' ' | COLOR_PAIR(1));
+                    break;
+                case '1':
+                    mvwaddch(GameBoard, i, j, '#' | COLOR_PAIR(5));
+                    break;
+                case '2':
+                    mvwaddch(GameBoard, i, j, '*' | COLOR_PAIR(4));
+                    break;
+                case '3':
+                    mvwaddch(GameBoard, i, j, '*' | COLOR_PAIR(6));
+                    break;
+                
+            }
+        }
+        wrefresh(GameBoard);
+    }
+
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+    if(key_thread.joinable())
+        key_thread.join();
 
     return 0;
 }
-
 
